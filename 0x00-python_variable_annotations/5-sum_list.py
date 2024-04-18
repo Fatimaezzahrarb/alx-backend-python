8f6369c3a57fd99adcff6f0f6f58b4f6e12bf054
Fatimaezzahrarb <132583542+Fatimaezzahrarb@users.noreply.github.com>
2024-04-18 23:03:06 +0100

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" Complex types """
-from typing import Callable, Iterator, Union, Optional, List
+'''Task 5.
+'''
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-   
-
-    return sum(input_list)
+    '''Computes sum of list of floating point numbers.
+    '''
+    return float(sum(input_list))
